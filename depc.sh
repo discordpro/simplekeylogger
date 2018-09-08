bf4dce9c23a572cc9af9c51d6b2a9cc2e53013a2
discordpro <discordpro01@gmail.com>
2018-09-08 01:14:13 -0400

    Fixed a typo

--- depc.sh
+++ depc.sh
@@ -1,7 +1,62 @@
-sudo apt-get install python-xlib
-sudo apt-get install git
-sudo md /kevm
-cd /kevm
-sudo git clone https://github.com/discordpro/simplekeylogger.git
-cd simplekeylogger
-sudo python keylog.py
+Script started on 2018-09-08 01:11:45-0400
+]0;pz@pz: ~/Desktop[01;32mpz@pz[00m:[01;34m~/Desktop[00m$ sh[K[Ksh
+$ 
+$ s
+sh: 2: s: not found
+$ d
+sh: 3: d: not found
+$ depc
+sh: 4: depc: not found
+$ depc.sh
+sh: 5: depc.sh: not found
+$ exit
+]0;pz@pz: ~/Desktop[01;32mpz@pz[00m:[01;34m~/Desktop[00m$ sh s[Kdepc.sh
+]0;pz@pz: ~/Desktop[01;32mpz@pz[00m:[01;34m~/Desktop[00m$ py
+
+Command 'py' not found, but can be installed with:
+
+sudo apt install pythonpy
+
+]0;pz@pz: ~/Desktop[01;32mpz@pz[00m:[01;34m~/Desktop[00m$ python
+Python 2.7.15rc1 (default, Apr 15 2018, 21:51:34) 
+[GCC 7.3.0] on linux2
+Type "help", "copyright", "credits" or "license" for more information.
+>>> exit
+Use exit() or Ctrl-D (i.e. EOF) to exit
+>>> exit()
+]0;pz@pz: ~/Desktop[01;32mpz@pz[00m:[01;34m~/Desktop[00m$ sudo apt-get install python-xlib
+[sudo] password for pz: 
+Reading package lists... 0%Reading package lists... 100%Reading package lists... Done
+Building dependency tree... 0%Building dependency tree... 0%Building dependency tree... 50%Building dependency tree... 50%Building dependency tree       
+Reading state information... 0%Reading state information... 0%Reading state information... Done
+The following NEW packages will be installed:
+  python-xlib
+0 upgraded, 1 newly installed, 0 to remove and 289 not upgraded.
+Need to get 0 B/111 kB of archives.
+After this operation, 784 kB of additional disk space will be used.
+Selecting previously unselected package python-xlib.
+(Reading database ... (Reading database ... 5%(Reading database ... 10%(Reading database ... 15%(Reading database ... 20%(Reading database ... 25%(Reading database ... 30%(Reading database ... 35%(Reading database ... 40%(Reading database ... 45%(Reading database ... 50%(Reading database ... 55%(Reading database ... 60%(Reading database ... 65%(Reading database ... 70%(Reading database ... 75%(Reading database ... 80%(Reading database ... 85%(Reading database ... 90%(Reading database ... 95%(Reading database ... 100%(Reading database ... 133398 files and directories currently installed.)
+Preparing to unpack .../python-xlib_0.20-3_all.deb ...
+Unpacking python-xlib (0.20-3) ...
+Setting up python-xlib (0.20-3) ...
+]0;pz@pz: ~/Desktop[01;32mpz@pz[00m:[01;34m~/Desktop[00m$ sudo apt-get install git
+Reading package lists... 0%Reading package lists... 100%Reading package lists... Done
+Building dependency tree... 0%Building dependency tree... 0%Building dependency tree... 50%Building dependency tree... 50%Building dependency tree       
+Reading state information... 0%Reading state information... 0%Reading state information... Done
+The following additional packages will be installed:
+  git-man liberror-perl
+Suggested packages:
+  git-daemon-run | git-daemon-sysvinit git-doc git-el git-email git-gui gitk
+  gitweb git-cvs git-mediawiki git-svn
+The following NEW packages will be installed:
+  git git-man liberror-perl
+0 upgraded, 3 newly installed, 0 to remove and 289 not upgraded.
+Need to get 4,731 kB of archives.
+After this operation, 33.9 MB of additional disk space will be used.
+Do you want to continue? [Y/n] y
+0% [Working]            Get:1 http://us.archive.ubuntu.com/ubuntu bionic/main amd64 liberror-perl all 0.17025-1 [22.8 kB]
+0% [1 liberror-perl 11.1 kB/22.8 kB 49%]                                        7% [Working]            Get:2 http://us.archive.ubuntu.com/ubuntu bionic-updates/main amd64 git-man all 1:2.17.1-1ubuntu0.1 [803 kB]
+7% [2 git-man 5,429 B/803 kB 1%]                                27% [Working]             Get:3 http://us.archive.ubuntu.com/ubuntu bionic-updates/main amd64 git amd64 1:2.17.1-1ubuntu0.1 [3,905 kB]
+27% [3 git 10.5 kB/3,905 kB 0%]56% [3 git 1,724 kB/3,905 kB 44%]82% [3 git 3,232 kB/3,905 kB 83%]                                 100% [Working]              Fetched 4,731 kB in 2s (2,998 kB/s)
+Selecting previously unselected package liberror-perl.
+(Reading database ... (Reading database ... 5%
